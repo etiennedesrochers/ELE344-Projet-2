7df5e2c528829a02fc7529335fcf8896a963ef90
Etienne Desrochers <desrocherset@gmail.com>
2024-06-21 13:17:09 -0400

    Modification du test best du controlleur

--- Controlleur Model Sim/control_tb.vhd
+++ Controlleur Model Sim/control_tb.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.std_logic_textio.ALL;
 USE std.textio.ALL;
-
+-- vsim -gui work.control_tb -do "add wave -r *;run 500ns"
 ENTITY control_tb IS
   GENERIC (TAILLE : integer := 32);
 END control_tb;
@@ -20,8 +20,30 @@ BEGIN
 
 process 
 begin
-    OP <= "000000";
-    Funct <= "100000";
-    wait for 20 ns;
+   --ALUOP 00
+   OP <= "100011"; 
+   Funct <= "000000";
+   wait for 20 ns;
+   --ALUOP 01
+   OP <= "000100"; 
+   Funct <= "000000";
+   wait for 20 ns;
+   --ALUOP 10
+   OP <= "000000"; 
+      --FUNCT 100000
+      Funct <= "100000";
+      wait for 20 ns;
+      --FUNCT 100010
+      Funct <= "100010";
+      wait for 20 ns;
+      --FUNCT 100100
+      Funct <= "100100";
+      wait for 20 ns;
+      --FUNCT 100101
+      Funct <= "100101";
+      wait for 20 ns;
+      --FUNCT 101010
+      Funct <= "101010";
+      wait for 20 ns;
 end process;
 end control_tb_arc;
