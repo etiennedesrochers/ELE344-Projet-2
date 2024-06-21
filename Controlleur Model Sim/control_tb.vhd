fa3b23b7fcc0b8856a77e53174f6ff6a05c6f6bf
Etienne Desrochers <desrocherset@gmail.com>
2024-06-21 13:45:06 -0400

    Modifcation test bench

--- Controlleur Model Sim/control_tb.vhd
+++ Controlleur Model Sim/control_tb.vhd
@@ -23,27 +23,35 @@ begin
    --ALUOP 00
    OP <= "100011"; 
    Funct <= "000000";
+
+   ASSERT AluControl /= "0010" report "Erreur ALUCOntrol incorrect"; 
    wait for 20 ns;
    --ALUOP 01
    OP <= "000100"; 
    Funct <= "000000";
+   ASSERT AluControl /= "0110" report "Erreur ALUCOntrol incorrect"; 
    wait for 20 ns;
    --ALUOP 10
    OP <= "000000"; 
       --FUNCT 100000
       Funct <= "100000";
+      ASSERT AluControl /= "0010" report "Erreur ALUCOntrol incorrect"; 
       wait for 20 ns;
       --FUNCT 100010
       Funct <= "100010";
+      ASSERT AluControl /= "0110" report "Erreur ALUCOntrol incorrect"; 
       wait for 20 ns;
       --FUNCT 100100
       Funct <= "100100";
+      ASSERT AluControl /= "0000" report "Erreur ALUCOntrol incorrect"; 
       wait for 20 ns;
       --FUNCT 100101
       Funct <= "100101";
+      ASSERT AluControl /= "0001" report "Erreur ALUCOntrol incorrect"; 
       wait for 20 ns;
       --FUNCT 101010
       Funct <= "101010";
+      ASSERT AluControl /= "0111" report "Erreur ALUCOntrol incorrect"; 
       wait for 20 ns;
 end process;
 end control_tb_arc;
